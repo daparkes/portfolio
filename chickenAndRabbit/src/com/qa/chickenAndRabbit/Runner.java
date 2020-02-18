9579216c31a2da2f442360e07f9d27799efbeb60
Dan Parkes <dan.parkes15@gmail.com>
2020-02-18 22:54:33 +0000

    added a solution to chickensandrabbits that doesn't work

--- chickenAndRabbit/src/com/qa/chickenAndRabbit/Runner.java
+++ chickenAndRabbit/src/com/qa/chickenAndRabbit/Runner.java
@@ -2,6 +2,15 @@ package com.qa.chickenAndRabbit;
 
 public class Runner {
 	public static void main(String[] args) {
-		
+		chickenAndRabbits(35, 94);
+	}
+	
+	public static void chickenAndRabbits(int heads, int legs) {
+		int chickenNumber = (heads*2) - (legs/2);
+		int rabbitNumber = chickenNumber-heads;
+		if (chickenNumber >= 0 && rabbitNumber >= 0) {
+			System.out.println("Number of chickens: " + chickenNumber + "\nNumber of rabbits: "
+					+ rabbitNumber);
+		}
 	}
 }
