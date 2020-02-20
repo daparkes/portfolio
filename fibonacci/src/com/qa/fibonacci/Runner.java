fe3a2b900f37a24aeff0667536b5a3554dd0f894
Dan Parkes <dan.parkes15@gmail.com>
2020-02-20 19:49:11 +0000

    finished fibonacci code

--- fibonacci/src/com/qa/fibonacci/Runner.java
+++ fibonacci/src/com/qa/fibonacci/Runner.java
@@ -1,16 +1,19 @@
 package com.qa.fibonacci;
 
-public class fibonacci {
+public class Runner {
 
 	public static void main(String[] args) {
-		
+		System.out.println(sequence(8));
 	}
+	
 	public static int sequence(int pos) {
-		for (int i = 1, j = 0, x=1; i<=pos;i++) {
+		int j = 0;
+		int i = 1;
+		for (int x=1; i<=pos; i++) {
 			int temp = j;
 			j = x + j;
 			x = temp;
 		}
-		return 
+		return j;
 	}
 }
